2009c7fe9f9ed754d7e44935c8df0f1ab4234f5c
FMG <funtasticaarav@gmail.com>
2025-08-08 10:07:49 +0530

    changed natural spawning

--- src/main/java/io/github/FMG9167/endy/entity/ModEntityGeneration.java
+++ src/main/java/io/github/FMG9167/endy/entity/ModEntityGeneration.java
@@ -1,5 +1,6 @@
 package io.github.FMG9167.endy.entity;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModification;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.SpawnGroup;
@@ -9,5 +10,6 @@ public class ModEntityGeneration {
     public static void addSpawns() {
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.THE_END, BiomeKeys.END_MIDLANDS, BiomeKeys.END_HIGHLANDS, BiomeKeys.END_BARRENS, BiomeKeys.SMALL_END_ISLANDS), SpawnGroup.MONSTER,
                 ModEntities.ENDY, 1, 1, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.all(), SpawnGroup.MONSTER, ModEntities.ENDY, 30, 1, 2);
     }
 }
